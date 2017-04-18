eec2917c7679653f5be1fc9ccaeaf75fdc5f2227
Qiang <fuqiang@outlook.com>
2017-04-18 11:42:30 +0800

    de dup

--- mnist/index.py
+++ mnist/index.py
@@ -1,8 +1,10 @@
 import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 import tensorflow as tf
 
+# data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# train
 x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
@@ -17,6 +19,7 @@ for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
+# check
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
