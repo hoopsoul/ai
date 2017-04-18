986feb066e28486ba3162276c0ce11b818660c47
Qiang <fuqiang@outlook.com>
2017-04-18 11:08:25 +0800

    add some tf demos

--- deep/1.py
+++ deep/1.py
@@ -8,15 +8,21 @@ import numpy as np
 x_data = np.float32(np.random.rand(2, 100)) # 随机输入
 y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
+print x_data, y_data
+
 # 构造一个线性模型
 b = tf.Variable(tf.zeros([1]))
 W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
 y = tf.matmul(W, x_data) + b
 
+print b, W, y
+
 # 最小化方差
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
+
+print loss, optimizer, train
 
 # 初始化变量
 init = tf.global_variables_initializer()
